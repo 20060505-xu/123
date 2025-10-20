5755721a3f616d9d9f022251cbf9b31bfdd85f5d
20060505-xu <18108133961@163.com>
2025-10-20 22:20:01 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,9 +1,13 @@
-//
-//
-//
+//202511716212
+//18108133961@163.com
+//徐任鸿
 #include <stdio.h>
 int main()
 {
-    printf("Hello World");
-    return 0;
+  float a;
+  float b;
+  scanf("%f",&a);
+   b =a*9/5+32;
+  printf("%f/n",b);
+  return 0;
 }
