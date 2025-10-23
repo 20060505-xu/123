8d2c498728ed5387c1bd172bce7b576bd557a414
20060505-xu <18108133961@163.com>
2025-10-23 14:16:47 +0800

    Create 4main.c

---
+++ 4main.c
@@ -0,0 +1,33 @@
+//202511716212
+//18108133961@163.com
+//徐任鸿
+#include <stdio.h>
+int main()
+{
+ int a, b, c;
+ scanf("%d %d %d",&a, &b, &c);
+ if (a<=0|| b<=0||c<=0)
+ { 
+    printf("无效");
+ }
+ else if (a + b> c&&a + c> b&&c + b>a )
+ {
+    printf("能组成三角形");
+ }
+else{
+ 
+     printf("不能形成三角形");
+ }
+ 
+    return 0;
+
+ 
+
+
+
+
+
+
+
+
+}
