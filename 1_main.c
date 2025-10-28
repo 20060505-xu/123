0fed7db08023cf2198276a67bdbd419b2b343a8f
20060505-xu <18108133961@163.com>
2025-10-28 20:09:36 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -1,36 +1,37 @@
 //202511716212
 //18108133961@163.com
 //徐任鸿
-#include<stdio.h>
-int main()
-{
-int n, i;
-int isPrime=1;
+#include <stdio.h>
 
-scanf("%d",&n);
+int main() {
+    int n, i;
+    int isPrime = 1; // 假设是质数
 
-if(n<=1||n>50)
-{
-    printf("输入无效");
+    printf("请输入一个小于50的正整数：");
+    scanf("%d", &n);
+
+    // 检查输入范围
+    if (n <= 1 || n >= 50) {
+        printf("输入无效，请输入1到49之间的正整数\n");
+        return 0;
+    }
+
+    // 判断是否为质数
+    for (i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            isPrime = 0; // 说明能被整除，不是质数
+            break;
+        }
+    }
+
+    // 输出结果
+    if (isPrime)
+        printf("密钥安全，密码设置成功\n");
+    else
+        printf("密钥不安全，请重新输入\n");
+
     return 0;
 }
-for ( i = 2; i<=n/2; i++)
-{  
-     if (n%i==0)
-
-{
-    isPrime=0;
-    break;
-}
-
-    
-}
-if(isPrime)
-  printf("密钥安全，密码设置成功");
-else
-  printf("密钥不安全，请重新输入");
-
-  return 0;
 
 
 
@@ -44,6 +45,4 @@ else
 
 
 
-
-}
 
