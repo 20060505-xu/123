aab613c02367789b6e066fe94e60be78161a118a
20060505-xu <18108133961@163.com>
2025-11-03 18:14:33 +0800

    Update 1_main.c

--- 1_main.c
+++ 1_main.c
@@ -5,21 +5,20 @@
 int main()
 {
 int a[3][3];
-int i;
-int j;
-for ( i = 0; i < 3; i++)
+
+for ( int i = 0; i < 3; i++)
 {
-    for (j= 0; j<3; j++)
+    for (int j= 0; j<3; j++)
     {
         scanf("%d", &a[i][j]);
     }
     
 }
-for ( i = 0; i <3; i++)
+for ( int i = 0; i <3; i++)
 {
-    for ( j= 0; j < 3; j++)
+    for (int j= 0; j < 3; j++)
     {
-        printf("%d", a[i][j]);
+        printf("%d ", a[i][j]);
     }
     printf("\n");
 }
