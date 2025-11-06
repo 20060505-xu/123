8f414030a1bcd618d0015e198f119f9bf87f4dc9
20060505-xu <18108133961@163.com>
2025-11-06 20:02:01 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,37 +2,43 @@
 //18108133961@163.com
 //徐任鸿
 #include <stdio.h>
-
-int main() {
-    int n, i = 2;
-    int isPrime = 1;  // 标记是否为质数
-
-    printf("请输入一个小于50的正整数：");
-    scanf("%d", &n);
-
-    // 判断输入范围
-    if (n <= 1 || n >= 50) {
-        printf("输入无效，请输入1到50之间的正整数。");
-        return 0;
-    }
-
-    // 判断是否为质数（使用while循环）
-    while (i < n) {
-        if (n % i == 0) {
-            isPrime = 0;  // 能被整除，不是质数
-            break;
+void a(int arr[],int n)
+{
+int i;
+int j;
+int temp;
+for ( i = 0; i <n-1; i++)
+{
+    for ( j = 0; j < n-1-i; j++)
+    {
+        if (arr[j]>arr[j+1])
+        {
+            temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+            
         }
-        i++;
+        
     }
-
-    // 输出结果（中文逗号）
-    if (isPrime)
-        printf("密钥安全，密码设置成功");
-    else
-        printf("密钥不安全，请重新输入");
-
-    return 0;
+    
 }
+}
+int main()
+{
+  int arr[10];
+  int i;
+  for ( i = 0; i <10; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  a(arr, 10);
+   for ( i = 0; i <10; i++)
+   {
+    printf("%d ",arr[i]);
+   }
+   return 0;
+}
+  
 
 
 
@@ -42,6 +48,14 @@ int main() {
 
 
 
-         
 
- 
+
+
+
+
+
+
+
+
+
+
