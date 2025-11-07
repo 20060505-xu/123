832216bfd1cdb5f284338a74c124943441bbed18
20060505-xu <18108133961@163.com>
2025-11-07 14:50:57 +0800

    Update 6_main.c

--- 6_main.c
+++ 6_main.c
@@ -2,28 +2,44 @@
 //18108133961@163.com
 //徐任鸿
 #include<stdio.h>
+int sum(int arr[],int n)
+{
+int a=0;
+for ( int i = 0; i < n; i++)
+{
+    a+=arr[i];
+}
+return a;
+
+
+
+}
+int product(int arr[],int n)
+{
+int b=1;
+for ( int i = 0; i < n; i++)
+{
+    b*=arr[i];
+}
+return b;
+
+
+
+}
 int main()
 {
-    int a[5];
-    int num, i;
-    for ( i = 0; i <5; i++)
-    {
-        do
-        {
-            scanf("%d" ,&num);
-        } while (num%2!=0);
-        
-        a[i]=num;
-        printf("%d ", a[i]);
-    }
+int i;
+int arr[5];
 
-    return 0;
+for ( i = 0; i < 5; i++)
+{
+    scanf("%d", &arr[i]);
+}
+int p=sum (arr,5);
+int q=product(arr,5);
+printf("%d %d\n",p ,q);
+return 0;
 
-
-
-
-
-    
 
 
 
@@ -32,3 +48,20 @@ int main()
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
