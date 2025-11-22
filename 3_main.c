a0dcc2c2483408538888a92b2f135f390a6b3aa8
20060505-xu <18108133961@163.com>
2025-11-22 10:45:09 +0800

    Update 3_main.c

--- 3_main.c
+++ 3_main.c
@@ -2,60 +2,20 @@
 //18108133961@163.com
 //徐任鸿
 #include <stdio.h>
-void a(int arr[],int n)
-{
-int i;
-int j;
-int temp;
-for ( i = 0; i <n-1; i++)
-{
-    for ( j = 0; j < n-1-i; j++)
-    {
-        if (arr[j]>arr[j+1])
-        {
-            temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
-            
-        }
-        
-    }
-    
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+
+    swap(&x, &y);
+
+    printf("%d %d", x, y);
+
+    return 0;
 }
-int main()
-{
-  int arr[10];
-  int i;
-  for ( i = 0; i <10; i++)
-  {
-    scanf("%d", &arr[i]);
-  }
-  a(arr, 10);
-   for ( i = 0; i <10; i++)
-   {
-    printf("%d ",arr[i]);
-   }
-   return 0;
-}
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
